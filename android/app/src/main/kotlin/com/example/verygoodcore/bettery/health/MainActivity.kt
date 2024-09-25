16ebb6487587a067378ca1a38e47f3e226881bb6
Mozammal Hossain <mozammal.hossain.dev@gmail.com>
2024-09-25 16:40:35 +0600

    first commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/bettery/health/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.bettery_health
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:bettery_health/app/app.dart';
+import 'package:bettery_health/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
